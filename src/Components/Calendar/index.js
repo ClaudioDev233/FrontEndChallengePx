04e2634fec2a1e17c64bf13c1ebfa458e9095e3b
Claudio <claudio.junior233@gmail.com>
2022-02-11 17:44:23 -0300

    calendar setup

--- src/Components/Calendar/index.js
+++ src/Components/Calendar/index.js
@@ -1,12 +1,54 @@
+import { CalendarContent, CalendarHeader, CalendarBody } from "./styles";
+import Moment from "react-moment";
+import moment from "moment";
+import { useState, useEffect } from "react";
 export default function Calendar() {
+  const [month, setMonth] = useState(2);
+  const [dia, setDia] = useState(
+    Array.from(Array(moment(`2022-01`).daysInMonth()).keys())
+  );
+
+  function handleMonth() {
+    let mes = month;
+    mes++;
+    setMonth(mes);
+
+    let newDays = Array.from(
+      Array(moment(`2022-0${month}`).daysInMonth()).keys()
+    );
+    console.log(newDays);
+    setDia(newDays);
+  }
+
+  const localShort = moment.weekdaysShort();
+
   return (
     <>
-      <div>
-        <div>
-          <input type="button" value="<" /> <p> Janeiro</p>
-          <input type="button" value=">" />
-        </div>
-      </div>
+      <CalendarContent>
+        <CalendarHeader>
+          <input type="button" value="<" />
+          <p>Fevereiro</p>
+          <input
+            type="button"
+            value=">"
+            onClick={() => {
+              handleMonth();
+            }}
+          />
+        </CalendarHeader>
+        <CalendarBody>
+          {localShort.map((day) => (
+            <p>{day}</p>
+          ))}
+          {dia.map((dias) => {
+            return (
+              <>
+                <p>{dias + 1}</p>
+              </>
+            );
+          })}
+        </CalendarBody>
+      </CalendarContent>
     </>
   );
 }
---
+++ src/Components/Calendar/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CalendarContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const CalendarHeader = styled.div`
+  display: flex;
+`;
+
+export const CalendarBody = styled.div`
+  display: flex;
+`;
