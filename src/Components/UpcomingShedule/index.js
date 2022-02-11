7eed678fcc1026ce48e82cf2343b0ed309e0a51e
Claudio <claudio.junior233@gmail.com>
2022-02-11 08:33:40 -0300

    added shedule card

--- src/Components/UpcomingShedule/index.js
+++ src/Components/UpcomingShedule/index.js
@@ -1,19 +1,30 @@
+import {
+  SheduleCard,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardColor,
+} from "./styles";
+
 export default function UpcomingShedule() {
   return (
     <>
       <p>Listagem de Agendamentos</p>
 
-      <div className="Card">
-        <div className="Color"></div>
-        <div className="Header">
-          <p>Exames do servidor C</p>
-          <p>5 de Janeiro de, 2021 </p>
-          <p>10-15hs</p>
-        </div>
-        <div className="Description">
-          <p>Nesse Agendamento contém todos os exames do tipo C</p>
-        </div>
-      </div>
+      <SheduleCard>
+        <CardColor />
+        <CardContent>
+          <CardTitle>
+            <p>Exames do servidor C</p>
+            <p>5 de Janeiro de, 2021 </p>
+            <p>10-15hs</p>
+          </CardTitle>
+          <CardDescription>
+            <p>Nesse Agendamento contém todos os exames do tipo C</p>
+          </CardDescription>
+        </CardContent>
+        <span> &gt; </span>
+      </SheduleCard>
     </>
   );
 }
---
+++ src/Components/UpcomingShedule/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const SheduleCard = styled.div`
+  width: 722px;
+  height: 96px;
+  background-color: #cccccc;
+  display: flex;
+  align-items: center;
+`;
+
+export const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardTitle = styled.div`
+  display: flex;
+`;
+
+export const CardDescription = styled.div``;
+
+export const CardColor = styled.div`
+  width: 16px;
+  height: 96px;
+  background-color: #4cbc9a;
+`;
